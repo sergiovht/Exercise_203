9d81d91d918c7efd69660733ca60c94b8ab97dec
Sergio Von Horoch <sergiovonhoroch@Sergios-MacBook-Pro.local>
2019-10-10 16:32:16 -0400

    First commit

---
+++ src/main/java/com/example/demo/employee.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+public class employee {
+    private String first;
+    private String last;
+    private int ssn;
+    private int dob_m;
+    private int dob_d;
+    private int dob_y;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getDob_m() {
+        return dob_m;
+    }
+
+    public void setDob_m(int dob_m) {
+        this.dob_m = dob_m;
+    }
+
+    public int getDob_d() {
+        return dob_d;
+    }
+
+    public void setDob_d(int dob_d) {
+        this.dob_d = dob_d;
+    }
+
+    public int getDob_y() {
+        return dob_y;
+    }
+
+    public void setDob_y(int dob_y) {
+        this.dob_y = dob_y;
+    }
+}
